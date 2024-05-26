6568a3e29a6dd822df7c55e7d9711c63a4ff92a0
abdelwahied <abdelwahied@gmail.com>
2024-05-26 15:07:46 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function sayHello() {
+    console.log("Hello World!");
+    // 3 ways to see output
+    alert("Hello, World");
+    var result = confirm("Do you like the JavaScript?");
+    var userInput = prompt("Enter your name");
+}
